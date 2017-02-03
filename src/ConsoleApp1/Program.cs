a22b36facfb7d869c26ad5f3550a154a41474b0e
Fabrizio Steiner <fas@bbtsoftware.ch>
2017-02-03 08:36:52 +0100

    Test use of portable pdb with sourcelink.

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        int result = ClassLibrary1.Class1.TestXy(0);
+    }
+}
